8d310f534fed28936a7d0acde19dbb481a7c49dd
nicopun <niconicopun@protonmail.com>
2020-01-17 20:12:03 +0000

    seventeen関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+
+/*配列に含まれる整数が 17 で割り切れるものだけにする*/
+
 'use strict';
 
+/*整数が17で割り切れるかどうかを判定する*/
+function isMultipleOfSeventeen(n) {
+    return n % 17 === 0;
+}
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
